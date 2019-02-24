8128fb91d782e78b39711b6b4156d022a5a3aaec
bartuyesilbag <bartuyesilbag@gmail.com>
2019-02-24 22:11:24 +0300

    First Commit

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComp;
+
+    // Use this for initialization
+    void Start () {
+        textComp.text = "Hello World";
+        textComp.fontSize = 60;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
